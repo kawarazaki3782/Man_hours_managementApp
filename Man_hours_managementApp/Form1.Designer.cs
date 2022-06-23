076083db5b3fce17d2f0b8cc52375105ef330358
kawar <kawar@KAWARAZAKI_PC>
2022-06-24 05:42:21 +0900

    プロジェクト ファイルを追加します。

---
+++ Man_hours_managementApp/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Man_hours_managementApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Passwordlabel = new System.Windows.Forms.Label();
+            this.Usernamelabel = new System.Windows.Forms.Label();
+            this.LoginidtextBox = new System.Windows.Forms.TextBox();
+            this.PasswordtextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(417, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "工数管理アプリ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(473, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 46);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ログイン";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Passwordlabel
+            // 
+            this.Passwordlabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Passwordlabel.AutoSize = true;
+            this.Passwordlabel.Font = new System.Drawing.Font("Yu Gothic UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Passwordlabel.Location = new System.Drawing.Point(289, 364);
+            this.Passwordlabel.Name = "Passwordlabel";
+            this.Passwordlabel.Size = new System.Drawing.Size(147, 46);
+            this.Passwordlabel.TabIndex = 2;
+            this.Passwordlabel.Text = "パスワード";
+            this.Passwordlabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Usernamelabel
+            // 
+            this.Usernamelabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Usernamelabel.AutoSize = true;
+            this.Usernamelabel.Font = new System.Drawing.Font("Yu Gothic UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Usernamelabel.Location = new System.Drawing.Point(289, 271);
+            this.Usernamelabel.Name = "Usernamelabel";
+            this.Usernamelabel.Size = new System.Drawing.Size(155, 46);
+            this.Usernamelabel.TabIndex = 3;
+            this.Usernamelabel.Text = "ログインID";
+            this.Usernamelabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LoginidtextBox
+            // 
+            this.LoginidtextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LoginidtextBox.Font = new System.Drawing.Font("Yu Gothic UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LoginidtextBox.Location = new System.Drawing.Point(450, 271);
+            this.LoginidtextBox.Name = "LoginidtextBox";
+            this.LoginidtextBox.Size = new System.Drawing.Size(304, 52);
+            this.LoginidtextBox.TabIndex = 4;
+            // 
+            // PasswordtextBox
+            // 
+            this.PasswordtextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PasswordtextBox.Font = new System.Drawing.Font("Yu Gothic UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.PasswordtextBox.Location = new System.Drawing.Point(450, 364);
+            this.PasswordtextBox.Name = "PasswordtextBox";
+            this.PasswordtextBox.Size = new System.Drawing.Size(304, 52);
+            this.PasswordtextBox.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 562);
+            this.Controls.Add(this.PasswordtextBox);
+            this.Controls.Add(this.LoginidtextBox);
+            this.Controls.Add(this.Usernamelabel);
+            this.Controls.Add(this.Passwordlabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label Passwordlabel;
+        private Label Usernamelabel;
+        private TextBox LoginidtextBox;
+        private TextBox PasswordtextBox;
+    }
+}
