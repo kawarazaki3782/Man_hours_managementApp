8a2db3807695ddccd11e2dbaaae5564848935b36
kawarazaki3782 <kawarazaki3782@gmail.com>
2022-08-29 05:58:02 +0900

    工数編集画面SQL文作成

--- Man_hours_managementApp/Man_Hours_Management_Edit_Form.Designer.cs
+++ Man_hours_managementApp/Man_Hours_Management_Edit_Form.Designer.cs
@@ -97,6 +97,7 @@
             this.button3.TabIndex = 13;
             this.button3.Text = "削除";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button1
             // 
@@ -106,6 +107,7 @@
             this.button1.TabIndex = 7;
             this.button1.Text = "登録";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label5
             // 
--- Man_hours_managementApp/Man_Hours_Management_Edit_Form.cs
+++ Man_hours_managementApp/Man_Hours_Management_Edit_Form.cs
@@ -61,5 +61,88 @@ namespace Man_hours_managementApp
             man_Hours_Management_Form.Show();
             this.Close();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var connectionString = CommonUtil.GetConnectionString();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    using (var transaction = connection.BeginTransaction())
+                    using (var command = new SqlCommand() { Connection = connection, Transaction = transaction })
+                    {
+                        try
+                        {
+                            command.CommandText = @"DELETE FROM Costs WHERE id = @id";
+                            command.Parameters.Add(new SqlParameter("@id", this.Cost_id));
+                            command.ExecuteNonQuery();
+                            transaction.Commit();
+                            MessageBox.Show("工数を削除しました");
+                            Man_Hours_Management_Form man_Hours_Management = new Man_Hours_Management_Form();
+                            man_Hours_Management.Show();
+                            this.Close();
+                        }
+                        catch
+                        {
+                            transaction.Rollback();
+                        }
+                    }
+                }
+                catch
+                {
+                    throw;
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var connectionString = CommonUtil.GetConnectionString();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    using (var transaction = connection.BeginTransaction())
+                    using (var command = new SqlCommand() { Connection = connection, Transaction = transaction })
+                    {
+                        try
+                        {
+                            command.CommandText = @"UPDATE Costs SET registration_date = @registration_date, name = @name, cost = @cost WHERE id = @id";
+                            command.Parameters.Add(new SqlParameter("@registration_date", dateTimePicker1.Value));
+                            command.Parameters.Add(new SqlParameter("@name", textBox4.Text));
+                            command.Parameters.Add(new SqlParameter("@cost", float.Parse(textBox5.Text)));
+                            command.Parameters.Add(new SqlParameter("@id", this.Cost_id));
+
+                            command.ExecuteNonQuery();
+                            transaction.Commit();
+                            MessageBox.Show("工数を編集しました");
+                            Man_Hours_Management_Form man_Hours_Management = new Man_Hours_Management_Form();
+                            man_Hours_Management.Show();
+                            this.Close();
+                        }
+                        catch
+                        {
+                            transaction.Rollback();
+                            throw;
+                        }
+                    }
+                }
+                catch
+                {
+                    throw;
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
     }
 }
