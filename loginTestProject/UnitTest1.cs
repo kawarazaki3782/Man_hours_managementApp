01be3821c4bd37ab3f98f160beaedaad14a1c57e
kawarazaki3782 <kawarazaki3782@gmail.com>
2022-09-18 08:40:18 +0900

    テストプロジェクトの作成

---
+++ loginTestProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Man_hours_managementApp;
+
+namespace loginTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {   
+        [TestMethod]
+        public void TestMethod1()
+        {
+           
+        }
+    }
+}
---
+++ loginTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace loginTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        LoginForm loginForm = new LoginForm();
+        [TestMethod]
+        //³í‚ÉƒƒOƒCƒ“‚Å‚«‚é‚±‚Æ
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
