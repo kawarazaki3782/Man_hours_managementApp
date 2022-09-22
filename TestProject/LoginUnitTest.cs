b9266546ccfc86ce9b8fb6bc0f7908643d9e3243
kawarazaki3782 <kawarazaki3782@gmail.com>
2022-09-23 07:14:09 +0900

    LoginFormテスト作成

--- TestProject/LoginUnitTest.cs
+++ TestProject/LoginUnitTest.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.IO;
 
 
+
 namespace TestProject
 
 {
@@ -17,6 +18,7 @@ namespace TestProject
     public class LoginUnitTest
     {
         [TestMethod]
+        //ログインできることのテスト
         public void TestLoginMethod()
         {
             TextBox PasswordtextBox = new TextBox();
@@ -24,10 +26,51 @@ namespace TestProject
             TextBox LoginidtextBox = new TextBox();
             LoginidtextBox.Text = "honkon";
 
-            bool ret = false;
             UserService user = new UserService();
-            ret = user.Authenticate(PasswordtextBox, LoginidtextBox);
+            bool ret = user.Authenticate(PasswordtextBox, LoginidtextBox);
             Assert.IsTrue(ret);
+        }
+
+        [TestMethod]
+        //ログインIDが間違っていた場合にログインできないこと
+        public void TestLoginFailedMethod()
+        {
+            TextBox PasswordtextBox = new TextBox();
+            PasswordtextBox.Text = "honkon";
+            TextBox LoginidtextBox = new TextBox();
+            LoginidtextBox.Text = "test";
+
+            UserService user = new UserService();
+            bool ret = user.Authenticate(PasswordtextBox, LoginidtextBox);
+            Assert.IsFalse(ret);
+        }
+
+        [TestMethod]
+        //パスワードが間違っていた場合にログインできないこと
+        public void TestLoginFailed2Method()
+        {
+            TextBox PasswordtextBox = new TextBox();
+            PasswordtextBox.Text = "test";
+            TextBox LoginidtextBox = new TextBox();
+            LoginidtextBox.Text = "honkon";
+
+            UserService user = new UserService();
+            bool ret = user.Authenticate(PasswordtextBox, LoginidtextBox);
+            Assert.IsFalse(ret);
+        }
+
+        [TestMethod]
+        //ログインIDが未入力の場合にメッセージが表示されること
+        public void TestLoginIDnotEnteredMethod()
+        {
+            TextBox PasswordtextBox = new TextBox();
+            PasswordtextBox.Text = "honkon";
+            TextBox LoginidtextBox = new TextBox();
+            LoginidtextBox.Text = "";
+
+            UserService user = new UserService();
+            bool ret = user.Authenticate(PasswordtextBox, LoginidtextBox);
+            Assert.IsFalse(ret);
         }
     }
 }
