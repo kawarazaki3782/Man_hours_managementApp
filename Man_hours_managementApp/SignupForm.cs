cdbe2273615ea6ef84a682d6674f489a2f1ae37b
kawarazaki3782 <kawarazaki3782@gmail.com>
2022-10-17 00:44:52 +0900

    SignupTest作成

--- Man_hours_managementApp/SignupForm.cs
+++ Man_hours_managementApp/SignupForm.cs
@@ -39,14 +39,33 @@ namespace Man_hours_managementApp
 
         private void clear_button_Click(object sender, EventArgs e)
         {
-            UserSignupFormLogic userSignupFormLogic = new UserSignupFormLogic();
-            userSignupFormLogic.Clear(textBox2, comboBox1, textBox4, textBox5);
+            textBox2.Text = String.Empty;
+            comboBox1.Text = String.Empty;
+            textBox4.Text = String.Empty;
+            textBox5.Text = String.Empty;
         }
 
         private void register_button_Click(object sender, EventArgs e)
         {
-            UserSignupFormLogic userSignupFormLogic = new UserSignupFormLogic();
-            userSignupFormLogic.Register(textBox2, textBox4, textBox5, comboBox1, ep, this);
+            InputCheck.errorClear(ep);
+            InputCheck.isString(ep, "氏名",textBox2 , true);
+            InputCheck.isString(ep, "ログインID", textBox4 , true);
+            InputCheck.isString(ep, "パスワード", textBox5 , true);
+            InputCheck.IsOnlyAlphanumeri(ep, "ログインID", textBox4, true);
+            InputCheck.IsOnlyAlphanumeri(ep, "パスワード", textBox5, true);
+            InputCheck.RequiredHalfSize(ep, "ログインID", textBox4,  true);
+            InputCheck.RequiredHalfSize(ep, "パスワード", textBox5, true);
+
+            if (InputCheck.isError == true)
+            {
+                MessageBox.Show("入力に不備があるため登録できません");
+            }
+
+            else
+            {
+                UserSignupFormLogic userSignupFormLogic = new UserSignupFormLogic();
+                userSignupFormLogic.Register(textBox2.Text.ToString(), textBox4.Text.ToString(), textBox5.Text.ToString(), comboBox1.Text.ToString(), ep, this);
+            }
         }
     }
 }
---
+++ TestProject/UserSignupUnitTest.cs
@@ -0,0 +1,73 @@
+﻿using Man_hours_managementApp;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
+
+namespace TestProject
+{
+    /// <summary>
+    /// UserSignupFormLogicのテスト
+    /// </summary>
+    /// 
+    [TestClass]
+    public class UserSignupUnitTest
+    {
+        [TestMethod]
+        //新規登録ができることのテスト
+        public void UserSignupTest001_SignupMethod()
+        {
+
+            SignupForm signupForm = new SignupForm();
+
+            TextBox textBox2 = new TextBox();
+            textBox2.Text = "テストユーザー";
+
+            TextBox textBox4 = new TextBox();
+            textBox4.Text = "testUser";
+
+            TextBox textBox5 = new TextBox();
+            textBox5.Text = "password3782";
+
+            ComboBox comboBox1 = new ComboBox();
+           
+            ErrorProvider ep = new ErrorProvider();
+
+
+            UserSignupFormLogic userSignupFormLogic = new UserSignupFormLogic();
+            bool ret = userSignupFormLogic.Register(textBox2.Text.ToString(), textBox4.Text.ToString(), textBox5.Text.ToString(), comboBox1.Text.ToString(), ep, signupForm);
+            Assert.IsTrue(ret);
+        }
+
+        [TestMethod]
+        //新規登録に失敗すること
+        public void UserSignupTest002_SignupFailureMethod()
+        {
+            SignupForm signupForm = new SignupForm();
+
+            TextBox textBox2 = new TextBox();
+            textBox2.Text = "新規登録失敗";
+
+            TextBox textBox4 = new TextBox();
+            textBox4.Text = "新規登録失敗";
+
+            TextBox textBox5 = new TextBox();
+            textBox5.Text = "新規登録失敗";
+
+            ComboBox comboBox1 = new ComboBox();
+
+            ErrorProvider ep = new ErrorProvider();
+
+
+            UserSignupFormLogic userSignupFormLogic = new UserSignupFormLogic();
+            bool ret = userSignupFormLogic.Register(textBox2.Text.ToString(), textBox4.Text.ToString(), textBox5.Text.ToString(), comboBox1.Text.ToString(), ep, signupForm);
+            Assert.IsFalse(ret);
+
+        }
+    }
+}
